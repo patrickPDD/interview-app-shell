6af9c6cf686c9f0e72d1676b597caa2921e167db
irynaricker <iryna.yaremtso@gmail.com>
2016-08-28 23:16:04 -0700

    Add files via upload

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,9 @@
 import {observer} from 'mobx-react'; // <-- That's store based magic, for now just think about it as making the following component watch when our state store changes
 import AppStore from '../stores/AppStore';
 import AppBody from './AppBody';
+import NeoStore from '../stores/NeoStore';
+import NeoSelect from '../stores/NeoStore';
+import NeoBody from './NeoBody';
 
 module.exports = observer(() => {
     return (
@@ -10,6 +13,7 @@ module.exports = observer(() => {
                 <h1>{AppStore.exampleTitle}</h1>
             </header>
             <AppBody Store={AppStore} />
+            <NeoBody Store={NeoStore} />
         </main>
     )
 });
